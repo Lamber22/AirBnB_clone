28df44cf18567ce56726b08c6060ed8baecb329a
jackmarley254 <engndiritu1@gmail.com>
2023-12-09 07:44:35 +0300

    Unittest task 3

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines unittests for models/base_model.py.
 Unittest classes:
-TestBaseModel_instantiation
-TestBaseModel_save
-TestBaseModel_to_dict
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
 """
 import os
 import models
@@ -12,3 +12,11 @@ from datetime import datetime
 from time import sleep
 from models.base_model import BaseModel
 
+
+class TestBaseModel_instantiation(unitest.TestCase):
+    """Unittests for testing intantiation of the BaseModel class."""
+
+    def test_no_args_instances(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def 
